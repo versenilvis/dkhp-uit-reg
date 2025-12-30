861c1260d7d75c5d1b4ffc00e708928a0af7107d
verse91 <versedev.store@proton.me>
2025-12-30 22:36:27 +0700

    feat: tailwind css v4

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+	Visit <a target="_blank" href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
+<p class="text-3xl font-bold text-blue-600">Tailwind OK</p>
