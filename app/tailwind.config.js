5c687eae5d5da0cf37e62b7a8dc13fc5bf840411
verse91 <versedev.store@proton.me>
2026-01-01 19:18:05 +0700

    feat: package and config

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,7 +3,30 @@
 export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                primary: '#FFD100',
+                background: '#FFD100',
+                'main-foreground': 'var(--main-foreground)',
+                'main': 'var(--main)',
+                'border': 'var(--border)',
+                'secondary-background': 'var(--secondary-background)',
+                'foreground': 'var(--foreground)'
+            },
+            borderRadius: {
+                'base': '0.5rem'
+            },
+            boxShadow: {
+                'shadow': 'var(--shadow)'
+            },
+            translate: {
+                'boxShadowX': 'var(--boxShadowX)',
+                'boxShadowY': 'var(--boxShadowY)',
+                'reverseBoxShadowX': 'var(--reverseBoxShadowX)',
+                'reverseBoxShadowY': 'var(--reverseBoxShadowY)'
+            }
+        }
     },
-    plugins: []
+    plugins: [require('tailwindcss-animate')]
+
 };
