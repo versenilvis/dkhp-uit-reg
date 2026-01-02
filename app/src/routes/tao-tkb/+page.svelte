591ae96e59b84134c38d21a466bba027428a25cd
verse91 <versedev.store@proton.me>
2026-01-03 01:00:44 +0700

    feat(page): tao tkb page

---
+++ app/src/routes/tao-tkb/+page.svelte
@@ -0,0 +1,10 @@
+<!-- This page is just an empty shell because the actual content is in PersistentTkb in +layout.svelte -->
+<!-- This helps switch routes immediately because no rendering is required -->
+<!-- I had some trouble finding a better solution because switching the route back to /tao-tkb renders the page very slowly -->
+<script lang="ts">
+	import Background from '$lib/components/Background.svelte';
+</script>
+
+<main>
+	<Background />
+</main>
